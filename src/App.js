0a35b9b1d1aef87001075864ced9e77cbb027572
Rohit <rrajak7499@gmail.com>
2021-10-12 19:47:59 +0530

    Added navigation menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import NavMenu from './components/NavMenu';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="d-flex">
+        <NavMenu title="Hello" link="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100"/>
+        <NavMenu title="Grocery" link="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100"/>
+        <NavMenu title="Mobiles" link="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100"/>
+        <NavMenu title="Fashon" link="https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100"/>
+        <NavMenu title="Electronics" link="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100"/>
+        <NavMenu title="Home" link="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100"/>
+        <NavMenu title="Appliances" link="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100"/>
+        <NavMenu title="Travel" link="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100"/>
+        <NavMenu title="Toys & More" link="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100"/>
+      </div>
     </>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function NavMenu(props) {
+    return (
+        <div className="InyRMC _3Il5oO">
+            <div className="_37M3Pb">
+                <div className="eFQ30H">
+                    <a href="/"></a>
+                    <div className="_1mkliO">
+                        <div className="CXW8mj">
+                            <img className="menu-image" src={props.link} alt="top" />
+                        </div>
+                    </div>
+                    
+                <div class="xtXmba">{props.title}</div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
